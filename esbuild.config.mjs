ae6f13cb1a9898d0aa15b8009cab3d57339f1b26
JKaypa <jose_kaypa@hotmail.com>
2025-07-22 18:29:09 -0500

    Refactor esbuild configuration to support development and production modes

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,9 +1,21 @@
 import * as esbuild from 'esbuild';
+import { argv } from 'node:process'
 
-let result = await esbuild.build({
+const isDev = argv.includes('--dev')
+const outdir = isDev ? './public/javascript' : '.build/public/javascript'
+
+const options = {
     entryPoints: ['./public/typescript/game.ts', './public/typescript/signin.ts'],
     bundle: true,
-    outdir: './public/javascript'
-});
+    minify: isDev,
+    outdir
+}
 
-console.info(result);
+if(isDev){
+    const ctx = await esbuild.context(options)
+    await ctx.watch()
+    console.info('esbuild is watching for changes...')
+} else {
+    await esbuild.build(options)
+    console.info('esbuild build completed successfully');
+}
