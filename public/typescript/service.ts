3e4bde05e2e397142409a47fb3b05f7f686bd71a
JKaypa <jose_kaypa@hotmail.com>
2025-07-26 18:16:47 -0500

    Set server URL dynamically based on environment

--- public/typescript/service.ts
+++ public/typescript/service.ts
@@ -1,3 +1,6 @@
-const server = 'http://localhost:3001';
+process.loadEnvFile()
+
+const isDev = process.env.NODE_ENV === 'development';
+const server = isDev ? 'http://localhost:3001' : 'https://okay-steffi-kaypa-dev-4c35edc0.koyeb.app';
 
 export { server };
