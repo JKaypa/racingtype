148a1d81a4f528106c5bc2f3459b3445d4fc4b6e
JKaypa <jose_kaypa@hotmail.com>
2025-07-29 13:59:12 -0500

    Remove unnecessary node loadEnvFile function

--- public/typescript/service.ts
+++ public/typescript/service.ts
@@ -1,5 +1,3 @@
-process.loadEnvFile()
-
 const isDev = process.env.NODE_ENV === 'development';
 
 const server = isDev ? 'http://localhost:3001' : 'https://okay-steffi-kaypa-dev-4c35edc0.koyeb.app';
