9b9fa86f900cd1cc152039625d36d7902e986363
JKaypa <jose_kaypa@hotmail.com>
2025-07-29 13:57:45 -0500

    Fix esbuild config: correct minify logic and add missing semicolon

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -4,15 +4,15 @@ import { argv } from 'node:process'
 const isDev = argv.includes('--dev')
 const outdir = isDev ? './public/javascript' : './build/public/javascript'
 
-const options = {
+const options= {
     entryPoints: ['./public/typescript/game.ts', './public/typescript/signin.ts'],
     bundle: true,
-    minify: !isDev,
+    minify: isDev,
     outdir
 }
 
 if(isDev){
-    const ctx = await esbuild.context(options)
+    const ctx = await esbuild.context(options);
     await ctx.watch()
     console.info('esbuild is watching for changes...\n')
 } else {
