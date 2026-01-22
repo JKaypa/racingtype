6d0485b7cf443f47cf634919e24e2641d6ad065f
JKaypa <jose_kaypa@hotmail.com>
2026-01-22 11:39:23 -0500

    minor change in brackets

--- public/typescript/views/room.ts
+++ public/typescript/views/room.ts
@@ -1,7 +1,7 @@
 import { createElement } from '../helpers/dom-helper.js';
 import { AppendRoom } from '~/types/types.js';
 
-const appendRoomElement = ({ name, numberOfUsers, onJoin = () => { } }: AppendRoom) => {
+const appendRoomElement = ({ name, numberOfUsers, onJoin = () => {} }: AppendRoom) => {
     const roomsContainer = <HTMLElement>document.querySelector('#rooms-wrapper');
 
     const nameElement = createElement({
