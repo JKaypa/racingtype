507e78f94f5452339678d49f9e3fa8b164125b07
JKaypa <jose_kaypa@hotmail.com>
2026-01-27 08:48:35 -0500

    Replace `onClose` export with `resetRoom` in handlers module.

--- public/typescript/handlers/handlers.ts
+++ public/typescript/handlers/handlers.ts
@@ -2,4 +2,4 @@ export { updateRoomsView } from './update-rooms-view/update-rooms.handler.js';
 export { submit } from './submit/submit.handler.js';
 export { showActiveRoom } from './active-room/active-room.handler.js';
 export { startingGame } from './starting-game/starting-game.handler.js';
-export { onClose } from './onClose/onClose.handler.js';
+export { resetRoom } from './reset-room/reset-room.handler.js';
